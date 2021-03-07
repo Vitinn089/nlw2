92808b9fb1be8f46c0724e44015486360fa20c59
Vitor Lopes <vitorlopes089@gmail.com>
2021-03-06 21:34:59 -0300

    Mais funções na comunicação com o Banco de Dados.

--- src/database/test.js
+++ src/database/test.js
@@ -12,7 +12,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química", 
+        subject: 1, 
         cost: "20,00"
         // o proffy_id vira pelo banco de dados
     }
@@ -31,7 +31,38 @@ Database.then(async (db) => {
         }
     ]
 
-    await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    // await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
     // Consultar dados inseridos
+
+    // Todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    // Consultar as classes de um determinado professor e trazer junto os dados do professor
+
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    // console.log(selectClassesAndProffys)
+
+    // o horario que a pessoa trabalha, por exemplo, é das 8h - 18
+    // o horário do time_from (8h) precisa ser antes ou igual ao horário solicitado
+    // o time_to precisa ser acima
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+
+    console.log(selectClassesSchedules)
+
+
 })
